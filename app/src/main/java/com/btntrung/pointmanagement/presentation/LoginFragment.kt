bd45f9bf4464613b98c755c14dca905c38a3edad
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-04-22 16:08:26 +0700

    (WIP) Add Firebase login

---
+++ app/src/main/java/com/btntrung/pointmanagement/presentation/LoginFragment.kt
@@ -0,0 +1,74 @@
+package com.btntrung.pointmanagement.presentation
+
+import android.R.attr
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.btntrung.pointmanagement.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
+
+class LoginFragment : Fragment() {
+    private lateinit var binding : FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonLoginGoogle.run {
+            setSize(SignInButton.SIZE_WIDE)
+            setOnClickListener {
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build()
+                val signInClient = GoogleSignIn.getClient(requireContext(), gso)
+                val signInIntent: Intent = signInClient.signInIntent
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode === RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
+        } catch (e: ApiException) {
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+        const val RC_SIGN_IN = 1
+    }
+}
