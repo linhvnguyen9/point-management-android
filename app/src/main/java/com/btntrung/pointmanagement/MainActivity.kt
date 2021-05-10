cc49b89b7ad2bcb41cbc4b5c1a5e20fab07ab344
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-05-10 22:12:34 +0700

    Add sharing feature

--- app/src/main/java/com/btntrung/pointmanagement/MainActivity.kt
+++ app/src/main/java/com/btntrung/pointmanagement/MainActivity.kt
@@ -2,11 +2,15 @@ package com.btntrung.pointmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.FacebookSdk
 import com.firebase.ui.auth.AuthUI
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Timber.d("Key hash: ${FacebookSdk.getApplicationSignature(this)}")
     }
 }
--- app/src/main/java/com/btntrung/pointmanagement/presentation/HomeFragment.kt
+++ app/src/main/java/com/btntrung/pointmanagement/presentation/HomeFragment.kt
@@ -1,18 +1,46 @@
 package com.btntrung.pointmanagement.presentation
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.btntrung.pointmanagement.R
+import androidx.fragment.app.Fragment
+import com.btntrung.pointmanagement.databinding.FragmentHomeBinding
+import com.facebook.share.model.SharePhoto
+import com.facebook.share.model.SharePhotoContent
+import com.facebook.share.widget.ShareDialog
 
 class HomeFragment : Fragment() {
+    private lateinit var binding : FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View{
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonHome.setOnClickListener {
+            val bitmap =
+                Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            view.draw(canvas)
+
+            val sharePhoto = SharePhoto.Builder()
+                .setBitmap(bitmap)
+                .build()
+
+            val content = SharePhotoContent.Builder()
+                .addPhoto(sharePhoto)
+                .build()
+            ShareDialog.show(this, content);
+        }
     }
 }
